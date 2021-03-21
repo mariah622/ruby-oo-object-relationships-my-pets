4b064f8b951ee75fd6c700fab4a21e0c3dc1ae6d
Mariah Morales <mjustine622@gmail.com>
2021-03-21 17:21:52 -0400

    commit tree f53230a5428073fcd42160dcdd2f41caf40a727e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+
+  end 
   # code goes here
+
+  def save
+    self.class.all << self
+  end 
+
+  def self.all
+    @@all 
+  end
 end
+#rspec spec/03_dog_spec.rb
